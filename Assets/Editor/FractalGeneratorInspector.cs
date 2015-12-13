a413c080bc63bcb124d020fa32cc9d9ceac74cdb
brian <cybergen@gmail.com>
2015-12-13 01:59:31 -0800

    Initial commit. Can generate a pyramid with messed up faces, so far.

---
+++ Assets/Editor/FractalGeneratorInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FractalGenerator))]
+public class FractalGeneratorInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate Mesh"))
+        {
+            (target as FractalGenerator).CreateMesh();
+        }
+    }
+}
---
+++ Assets/Scripts/FractalGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class FractalGenerator : MonoBehaviour
+{
+    public int FractalIterations;
+    public float BaseWidth;
+
+    public void CreateMesh()
+    {
+        Debug.Log("Generating mesh");
+
+        var mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+
+        var vertices = new Vector3[4];
+        var uvs = new Vector2[4];
+        var triangles = new int[12];
+
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(1, 0, 0);
+        vertices[2] = new Vector3(0.5f, 1, 0);
+        vertices[3] = new Vector3(0.5f, 0.5f, 1);
+        mesh.vertices = vertices;
+
+        uvs[0] = new Vector2(0, 0);
+        uvs[1] = new Vector2(1, 0);
+        uvs[2] = new Vector2(0.5f, 0.5f);
+        uvs[3] = new Vector2(1, 1);
+        mesh.uv = uvs;
+
+        triangles = new int[12] { 0, 2, 1, 0, 2, 3, 2, 1, 3, 0, 3, 1 };
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+    }
+}
