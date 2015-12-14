59c110fb000d6f3af1c0db7e0a07c52cafd33d2a
brian <cybergen@gmail.com>
2015-12-13 21:53:48 -0800

    More progress towards making an actual shape.

--- Assets/Scripts/FractalGenerator.cs
+++ Assets/Scripts/FractalGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
 public class FractalGenerator : MonoBehaviour
 {
     public int FractalIterations;
@@ -9,17 +11,19 @@ public class FractalGenerator : MonoBehaviour
     {
         Debug.Log("Generating mesh");
 
-        var mesh = GetComponent<MeshFilter>().mesh;
+        var mesh = GetComponent<MeshFilter>().sharedMesh;
         mesh.Clear();
 
         var vertices = new Vector3[4];
         var uvs = new Vector2[4];
         var triangles = new int[12];
 
+        var vertLeg = Mathf.Tan(Mathf.Deg2Rad * 60) * BaseWidth * 0.5f;
+
         vertices[0] = new Vector3(0, 0, 0);
-        vertices[1] = new Vector3(1, 0, 0);
-        vertices[2] = new Vector3(0.5f, 1, 0);
-        vertices[3] = new Vector3(0.5f, 0.5f, 1);
+        vertices[1] = new Vector3(BaseWidth, 0, 0);
+        vertices[2] = new Vector3(BaseWidth * 0.5f, 0, vertLeg);
+        vertices[3] = new Vector3(0.5f * BaseWidth, vertLeg, vertLeg * 0.5f);
         mesh.vertices = vertices;
 
         uvs[0] = new Vector2(0, 0);
