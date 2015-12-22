02042ffa0a89b4aba724d19599b0dd7466c93701
brian <cybergen@gmail.com>
2015-12-22 15:17:28 -0800

    Beginning to break triangle generation out into methods that can be called recursively

--- Assets/Scripts/FractalGenerator.cs
+++ Assets/Scripts/FractalGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
@@ -14,28 +16,38 @@ public class FractalGenerator : MonoBehaviour
         var mesh = GetComponent<MeshFilter>().sharedMesh;
         mesh.Clear();
 
-        var vertices = new Vector3[4];
-        var uvs = new Vector2[4];
-        var triangles = new int[12];
-
-        var vertLeg = Mathf.Tan(Mathf.Deg2Rad * 60) * BaseWidth * 0.5f;
-
-        vertices[0] = new Vector3(0, 0, 0);
-        vertices[1] = new Vector3(BaseWidth, 0, 0);
-        vertices[2] = new Vector3(BaseWidth * 0.5f, 0, vertLeg);
-        vertices[3] = new Vector3(0.5f * BaseWidth, vertLeg, vertLeg * 0.5f);
-        mesh.vertices = vertices;
-
-        uvs[0] = new Vector2(0, 0);
-        uvs[1] = new Vector2(1, 0);
-        uvs[2] = new Vector2(0.5f, 0.5f);
-        uvs[3] = new Vector2(1, 1);
-        mesh.uv = uvs;
-
-        triangles = new int[12] { 1, 2, 0, 0, 2, 3, 2, 1, 3, 0, 3, 1 };
-        mesh.triangles = triangles;
-
+        var vertices = new List<Vector3>();
+        var triangles = new List<int>();
+        AddTriangle(Vector3.zero, Vector3.up, vertices, triangles, BaseWidth);
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray(); 
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
+    }
+
+    private void AddTriangle(Vector3 center, Vector3 up, List<Vector3> currentVectors, List<int> triangles, float width)
+    {
+        var triangles = new int[12];
+
+        var halfWidth = width / 2;
+        var vertLeg = Mathf.Sqrt(Sq(width) - Sq(halfWidth));
+
+        var s = currentVectors.Count;
+        currentVectors.Add(new Vector3(0, 0, 0));
+        currentVectors.Add(new Vector3(width, 0, 0));
+        currentVectors.Add(new Vector3(width * 0.5f, 0, vertLeg));
+        currentVectors.Add(new Vector3(0.5f * width, vertLeg, vertLeg * 0.5f));
+
+        triangles = new int[12] {
+            s + 1, s + 2, s + 0,
+            s + 0, s + 2, s + 3,
+            s + 2, s + 1, s + 3,
+            s + 0, s + 3, s + 1 };
+        return triangles;
+    }
+
+    private static float Sq(float value)
+    {
+        return value * value;
     }
 }
