4cb3eff790822b19cd614678e7c1d745a14bd050
brian <cybergen@gmail.com>
2015-12-22 22:06:26 -0800

    Sort of a fractal!

--- Assets/Scripts/FractalGenerator.cs
+++ Assets/Scripts/FractalGenerator.cs
@@ -31,7 +31,8 @@ public class FractalGenerator : MonoBehaviour
         List<Vector3> vectors, 
         List<int> triangles, 
         float width,
-        float recursionDepth)
+        float recursionDepth,
+        bool recurseBottom = true)
     {
         var halfWidth = width / 2;
         var vertLeg = Mathf.Sqrt(Sq(width) - Sq(halfWidth));
@@ -44,11 +45,17 @@ public class FractalGenerator : MonoBehaviour
         var right = Vector3.Cross(up, forward);
 
         var s = vectors.Count;
-        vectors.Add(center + halfWidth * left + halfVert * back);
-        var bottomLeftPoint = center + halfWidth * left;
-        vectors.Add(center + halfWidth * right + halfVert * back);
-        vectors.Add(center + halfVert * forward);
-        vectors.Add(center + up * vertLeg);
+        var bottomFrontMidPoint = center + halfWidth * back;
+        var frontLeftPoint = center + halfWidth * left + halfVert * back;
+        vectors.Add(frontLeftPoint);
+        var frontRightPoint = center + halfWidth * right + halfVert * back;
+        vectors.Add(frontRightPoint);
+        var bottomTip = center + halfVert * forward;
+        var bottomLeftMidPoint = frontLeftPoint + (bottomTip - frontLeftPoint) / 2;
+        var bottomRightMidPoint = bottomTip + (frontRightPoint - bottomTip) / 2;
+        vectors.Add(bottomTip);
+        var top = center + up * vertLeg;
+        vectors.Add(top);
 
         triangles.AddRange(new List<int> {
             s + 1, s + 2, s + 0,
@@ -59,7 +66,17 @@ public class FractalGenerator : MonoBehaviour
         if (recursionDepth > 1)
         {
             var newDepth = recursionDepth - 1;
-            Draw(center, -up, back, vectors, triangles, width / 2, newDepth);
+            if (recurseBottom) Draw(center, -up, forward, vectors, triangles, width / 2, newDepth, false);
+
+            //Front tri
+            var frontForward = top - bottomFrontMidPoint;
+            var frontCent = bottomFrontMidPoint + frontForward / 2;
+            var frontUp = Vector3.Cross(frontRightPoint - frontLeftPoint, frontForward);
+            Draw(frontCent, -frontUp, frontForward, vectors, triangles, width / 2, newDepth, false);
+
+            //Left side tri
+
+            //Right side tri
         }
     }
 
