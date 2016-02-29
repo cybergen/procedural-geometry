67a3a93e4c5bf6496232bc6a9b931569b63601e9
brian <cybergen@gmail.com>
2016-02-29 08:56:45 -0800

    Made fractal iterations a parameter that can be supplied to the compute shader and made fractal generator supply it

--- Assets/Scripts/FractalGenerator2.cs
+++ Assets/Scripts/FractalGenerator2.cs
@@ -5,20 +5,19 @@ public class FractalGenerator2 : Generatable
     public ComputeShader TopologyCompute;
     public int FractalIterations;
     public Material AttachedMaterial;
+    public int TextureSize;
 
     public override void Generate()
     {
         var kernel = TopologyCompute.FindKernel("CSMain");
 
-        var displace = new RenderTexture(256, 256, 24);
+        var displace = new RenderTexture(TextureSize, TextureSize, 24);
         displace.enableRandomWrite = true;
         displace.Create();
-        var normal = new RenderTexture(256, 256, 24);
-        normal.enableRandomWrite = true;
-        normal.Create();
 
         TopologyCompute.SetTexture(kernel, "Displace", displace);
-        TopologyCompute.Dispatch(kernel, 256 / 8, 256 / 8, 1);
+        TopologyCompute.SetInt("Iterations", FractalIterations);
+        TopologyCompute.Dispatch(kernel, TextureSize / 8, TextureSize / 8, 1);
 
         AttachedMaterial.SetTexture("_Displacement", displace);
     }
