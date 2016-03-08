bf9788683ee3519e454d50f8a3b6abedc28fd309
brian <cybergen@gmail.com>
2016-03-07 23:20:24 -0800

    Added a bunch of logging via compute buffer. Shifted around start uv to be base -0.5 to 0.5. Experimenting with actually getting accurate y values, to start. Almost have something, it appears.

--- Assets/Scripts/FractalGenerator2.cs
+++ Assets/Scripts/FractalGenerator2.cs
@@ -8,9 +8,33 @@ public class FractalGenerator2 : Generatable
     public Material AttachedMaterial;
     public int TextureSize;
 
+    public struct PointData
+    {
+        float x;
+        float y;
+        float iteration;
+        float radius;
+        float logRadius;
+        float theta;
+        Vector2 spiral;
+        Vector2 uv;
+        Vector3 stepDisplacement;
+        Vector3 totalDirection;
+
+        public override string ToString()
+        {
+            return string.Format("[PointData x={0}, y={1}, iteration={8},radius={3}, logRadius={4}, theta={5}, spiral={6}, uv={9}, stepDisplacement={2},  totalDir={7}]",
+            x, y, stepDisplacement, radius, logRadius, theta, spiral, totalDirection, iteration, uv);
+        }
+    }
+
     public override void Generate()
     {
         var kernel = TopologyCompute.FindKernel("CSMain");
+
+        var structBuffer = new PointData[3];
+        var size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(PointData));
+        var cb = new ComputeBuffer(structBuffer.Length, size);
 
         var displace = new RenderTexture(TextureSize, TextureSize, 24);
         displace.enableRandomWrite = true;
@@ -20,6 +44,7 @@ public class FractalGenerator2 : Generatable
         normal.enableRandomWrite = true;
         normal.Create();
 
+        TopologyCompute.SetBuffer(kernel, "Data", cb);
         TopologyCompute.SetTexture(kernel, "Displace", displace);
         TopologyCompute.SetTexture(kernel, "Normal", displace);
         TopologyCompute.SetInt("Iterations", FractalIterations);
@@ -30,5 +55,12 @@ public class FractalGenerator2 : Generatable
         AttachedMaterial.SetTexture("_MainTex", displace);
         AttachedMaterial.SetTexture("_Displacement", displace);
         AttachedMaterial.SetTexture("_Normal", normal);
+
+        cb.GetData(structBuffer);
+
+        foreach (var data in structBuffer)
+        {
+            Debug.Log(data);
+        }
     }
 }
